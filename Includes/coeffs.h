e5ea6ea3acb8a54514acfba2b425b2289daeb2fe
lhhkkk <2794027306@qq.com>
2023-05-04 16:05:18 +0800

    修改了pid系数

--- Includes/coeffs.h
+++ Includes/coeffs.h
@@ -1,9 +1,9 @@
 #ifndef _COEFF_H_
 #define _COEFF_H_
 // 系数
-#define V_kP (350)
-#define V_kI (300)
-#define V_kD (100)
+#define V_kP (1900)
+#define V_kI (400)
+#define V_kD (600)
 #define V_k_20ms (139.35225)
 #define V_k_10ms (69.676125)
 #define V_k_1ms (6.9676125)
